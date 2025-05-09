29a0a247a828e0d8dc8a8be8b02a30d8745fd788
Esteban <esteban.heschung@etudiant.univ-rennes.fr>
2025-05-09 18:13:56 +0200

    Ajout des utilisateurs crée et fonctionnel

--- gestion.php
+++ gestion.php
@@ -4,7 +4,7 @@ $description = "Page de gestion";
 $keywords = "default";
 
 include("./scripts/functions.php");
-$data = json_decode(file_get_contents("../data/utilisateurs.json",true));
+$data = json_decode(file_get_contents("./data/utilisateurs.json"),true);
 
 parametres($page,$description,$keywords);
 
@@ -16,9 +16,9 @@ if (isset($_POST["name"])){
 
   echo("<pre>".print_r($_POST,true)."</pre>");
   $username = strtolower($_POST["prenom"])[0].strtolower($_POST["name"]);
-  $data[$username] = array("nom" = $_POST["name"],"prenom"=>$_POST["prenom"],"telephone"=>$_POST["tel"],"roles"=>array(),"password"=>password_hash($_POST["password"], PASSWORD_DEFAULT));
+  $data[$username] = array("nom" => $_POST["name"],"prenom"=>$_POST["prenom"],"telephone"=>$_POST["tel"],"roles"=>array(),"password"=>password_hash($_POST["password"], PASSWORD_DEFAULT));
   print_r($data);
-  file_put_contents("../data/utilisateurs.json", $data);
+  file_put_contents("./data/utilisateurs.json", json_encode($data));
 
 }
 else{
