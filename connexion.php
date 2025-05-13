38a3e50e943361f47805c467c00054935a8f6398
user <user@mRTe210158>
2025-05-13 12:14:49 +0200

    Début de la page de connexion

--- connexion.php
+++ connexion.php
@@ -1,27 +1,39 @@
 <?php
-session_start();
+$page = "Gestion des utilisateurs";
+$description = "Page de gestion";
+$keywords = "default";
 
-echo'  <div class="card-header text-center">
-            <h4>Connexion</h4>
-          </div>
-          <div class="card-body">
-            <form action="login.php" method="POST">
-              <!-- Champ Nom utilisateur -->
-              <div class="mb-3">
-                <label for="user" class="form-label">Nom d\'utilisateur :</label>
-                <input type="text" class="form-control" id="user" name="user" required>
-              </div>
-              
-              <!-- Champ Mot de passe -->
-              <div class="mb-3">
-                <label for="password" class="form-label">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-              </div>
-              
-              <!-- Bouton de connexion -->
-              <div class="d-flex justify-content-between">
-                <button type="submit" class="btn btn-primary">Se connecter</button>
-              </div>
-            </form>';
+include("./scripts/functions.php");
+$data = json_decode(file_get_contents("./data/utilisateurs.json"),true);
 
+parametres($page,$description,$keywords);
+
+entete($page);
+
+navigation($page);
+
+if (isset($_POST["username"]) && isset($data["username"]) && password_verify($_POST["pswd"], $data["username"]["password"])){
+  echo("validate");
+}
+else{
+  altert("Utilisateur non valide");
+}
+
+echo('
+<div class="container">
+<h2 class="mt-4">Connexion à l\'intranet</h2>
+<form action="#">
+  <div class="mb-3 mt-3">
+    <label for="username" class="form-label">Pseudo</label>
+    <input type="text" class="form-control" id="email" placeholder="Entrez votre pseudo" name="username">
+  </div>
+  <div class="mb-3">
+    <label for="pwd" class="form-label">Mot de passe :</label>
+    <input type="password" class="form-control" id="pwd" placeholder="Entrez votre mot de passe" name="pswd">
+  </div>
+  <button type="submit" class="btn btn-primary">Connexion</button>
+</form>
+</div>');
+
+pieddepage();
 ?>
