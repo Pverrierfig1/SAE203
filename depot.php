f3a5a61716e2450c14dca5b1c3bb6c62c1a4caee
Esteban <esteban.heschung@etudiant.univ-rennes.fr>
2025-05-14 18:25:32 +0200

    Réécriture d'un commentaire

--- depot.php
+++ depot.php
@@ -15,7 +15,7 @@ if (isset($_FILES["fichier"]) && $_FILES["fichier"]["error"] === UPLOAD_ERR_OK) 
 	if (!is_dir($path)){
 		mkdir($path); // on crée un dossier du nom de l'utilisateur si il n'existe pas
 	}
-	move_uploaded_file($_FILES["fichier"]["tmp_name"], $path."/".$_FILES["fichier"]["name"]); // on met dans le dossier de l'utilisateur qui est temporère
+	move_uploaded_file($_FILES["fichier"]["tmp_name"], $path."/".$_FILES["fichier"]["name"]); // on met dans le dossier de l'utilisateur le fichier temp
 }
 echo('
 <div class="container">
