78d0b17a48363c7af2e55465248833926badba3d
Kylian Cazorla <kylian.cazorla@etudiant.univ-rennes.fr>
2025-05-17 18:40:09 +0200

    Modification du bouton Hydrofix afin qu'il redirige vers la page d'accueil du site vitrine

--- scripts/functions.php
+++ scripts/functions.php
@@ -41,7 +41,7 @@ function navigation($page){
   echo( "
   <nav class='navbar navbar-expand-lg navbar-light bg-light'>
   <div class='container'>
-    <a class='navbar-brand' href='../accueil.php'>Hydrofix</a>
+    <a class='navbar-brand' href='../accueil_wordpress.html'>Hydrofix</a>
     <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
       <span class='navbar-toggler-icon'></span>
     </button>
