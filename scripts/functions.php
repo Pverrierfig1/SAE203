b67b475dcce6ca66d4524f282251367b17b19347
Esteban <esteban.heschung@etudiant.univ-rennes.fr>
2025-05-17 13:28:53 +0200

    Modification de la référence pour le logo (ca fonctionne)

--- scripts/functions.php
+++ scripts/functions.php
@@ -14,7 +14,7 @@ function parametres($titre,$description,$keywords){
   <meta name="author" content="Esteban Heschung">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="icon" href="/images/logo.png">
+  <link rel="icon" href="./images/logo.png">
 </head>
 <body>');
 }
