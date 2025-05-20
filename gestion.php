514b334b1cd0e846481ac714755139997fb86d64
user <user@mRTe210158>
2025-05-20 17:55:11 +0200

    Ajout de la gestion de la création de l'utilisateur et des bouttons qui redirigent vers les pages, il faut encore faire la suppresion des utilisateurs et la modification

--- gestion.php
+++ gestion.php
@@ -38,47 +38,52 @@ elseif(isset($_POST["ajouter"])){ //affichage de l'inscription de l'utilisateur
 //les roles sont sous forme de tableau pour ne pas avoir a les transformer a la l'envoi du form
   echo('
 <div class="container">
-<h1>Création d\'un utilisateur</h1>
- <form action="#" method="POST">
-  <div class="mb-3 mt-3">
-    <label for="name" class="form-label">Nom de l\'utilisateur</label>
-    <input type="text" class="form-control" id="name" placeholder="Entrez un nom" name="name" required> 
-  </div>
-  <div class="mb-3">
-    <label for="prenom" class="form-label">Entrez le prénom de l\'utilisateur</label>
-    <input type="text" class="form-control" id="prenom" placeholder="Entez un prénom" name="prenom" required>
-  </div>
+  <h1 class="mt-4">Création d\'un utilisateur</h1>
+    <form method="POST" action="#">
+      <a href="./gestion.php" class="btn btn-info">Tableau de bord</a>
+      <button type="submit" class="btn btn-info" name="ajouter">Ajout d\'employé</button>
+      <a href="./annuaire_entreprise.php" class="btn btn-info">Gérer les employés</a>
+     </form>
+   <form action="#" method="POST">
+    <div class="mb-3 mt-3">
+      <label for="name" class="form-label">Nom de l\'utilisateur</label>
+      <input type="text" class="form-control" id="name" placeholder="Entrez un nom" name="name" required> 
+    </div>
     <div class="mb-3">
-    <label for="tel" class="form-label">Entrez le numéro de téléphone de l\'utilisateur</label>
-    <input type="tel" class="form-control" id="tel" placeholder="Entez un numéro de téléphone" name="tel" required>
-  </div>
-  <div class="mb-3">
-    <label for="email" class="form-label">Entrez l\'e-mail de l\'utilisateur</label>
-    <input type="email" class="form-control" id="email" placeholder="Entez un e-mail" name="email" required>
-  </div>
-  <div class="form-check">
-      <h2>Séléctionnez les rôle de l\'utilisateur</h2>
-      <input type="checkbox" class="form-check-input" id="check1" name="roles[]" value="administrateur">
-      <label class="form-check-label" for="check1">Administrateur</label>
-  </div>
-  <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="check3" name="roles[]" value="manager">
-      <label class="form-check-label" for="check3">Manager</label>
-  </div>
-  <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="check4" name="roles[]" value="direction">
-      <label class="form-check-label" for="check4">Direction</label>
-  </div>
-  <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="check2" name="roles[]" value="salarie" checked>
-      <label class="form-check-label" for="check2">Salarié</label>
-  </div>
-  <div class="mb-3 mt-3">
-    <label for="password" class="form-label">Entrez le mot de passe de l\'utilisateur</label>
-    <input type="password" class="form-control" id="password" placeholder="Entez un mot de passe" name="password" required>
-  </div>
-  <button type="submit" class="btn btn-primary">Création</button>
-</form> 
+      <label for="prenom" class="form-label">Entrez le prénom de l\'utilisateur</label>
+      <input type="text" class="form-control" id="prenom" placeholder="Entez un prénom" name="prenom" required>
+    </div>
+      <div class="mb-3">
+      <label for="tel" class="form-label">Entrez le numéro de téléphone de l\'utilisateur</label>
+      <input type="tel" class="form-control" id="tel" placeholder="Entez un numéro de téléphone" name="tel" required>
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">Entrez l\'e-mail de l\'utilisateur</label>
+      <input type="email" class="form-control" id="email" placeholder="Entez un e-mail" name="email" required>
+    </div>
+    <div class="form-check">
+        <h2>Séléctionnez les rôle de l\'utilisateur</h2>
+        <input type="checkbox" class="form-check-input" id="check1" name="roles[]" value="administrateur">
+        <label class="form-check-label" for="check1">Administrateur</label>
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="check3" name="roles[]" value="manager">
+        <label class="form-check-label" for="check3">Manager</label>
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="check4" name="roles[]" value="direction">
+        <label class="form-check-label" for="check4">Direction</label>
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="check2" name="roles[]" value="salarie" checked>
+        <label class="form-check-label" for="check2">Salarié</label>
+    </div>
+    <div class="mb-3 mt-3">
+      <label for="password" class="form-label">Entrez le mot de passe de l\'utilisateur</label>
+      <input type="password" class="form-control" id="password" placeholder="Entez un mot de passe" name="password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Création</button>
+  </form> 
 </div>
   ');
 
@@ -92,10 +97,12 @@ elseif(isset($_POST["suppression"])){ //
 else{
   echo('
 <div class="container-fluid row">
-           <div class="btn-group-vertical col-1">
-            <button type="button" class="btn btn-info">Tableau de bord</button>
-            <button type="button" class="btn btn-info">Ajout d\'employé</button>
-            <a href="./annuaire_entreprise.php" class="btn btn-info">Gérer les employés</a>
+          <div class="btn-group-vertical col-1">
+            <form method="POST" action="#">
+              <a href="./gestion.php" class="btn btn-info">Tableau de bord</a>
+              <button type="submit" class="btn btn-info mt-2" name="ajouter">Ajout d\'employé</button>
+              <a href="./annuaire_entreprise.php" class="btn btn-info mt-2">Gérer les employés</a>
+            </form>
           </div> 
           <div class="row col text-center">
             <h2 class="mt-4">Tableau de bord</h1>
