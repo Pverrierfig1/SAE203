1bce76c5ef1746f794c14f5bd707ab78be48164f
matheeeooo <mathdeb35@gmail.com>
2025-05-20 14:03:58 +0200

    Update annuaire.php
    modif bio

--- annuaire.php
+++ annuaire.php
@@ -12,17 +12,17 @@ echo "<table border='1' cellpadding='10'>";
 echo "<tr><th>Photo</th><th>Nom</th><th>Prénom</th><th>Fonction</th><th>Bio</th></tr>";
 // Parcours de chaque utilisateur dans le tableau JSON
 foreach ($liste_util as $identifiant => $user) {  // $identifiant = clé (ex: jean.roland), $user = tableau des infos
-    $nom = htmlspecialchars($user['nom']);
-    $prenom = htmlspecialchars($user['prenom']);
-    $role = htmlspecialchars(implode(", ", $user['roles'])); // Le rôles) est un tableau, on le transforme en chaîne
-    // $bio = htmlspecialchars($user['bio']); // ligne en attente
+    $nom = $user['nom'];
+    $prenom = $user['prenom'];
+    $role = implode(", ", $user['roles']); // Le rôles est un tableau, on le transforme en chaîne
+    $bio = $user['bio']; 
     $photo = "images/images_utilisateur/image_defaut.jpg"; // image par défaut pour tous pour l'instant
     echo "<tr>";
     echo "<td><img src='images/images_utilisateur/image_defaut.jpg' alt='Photo de $prenom $nom' width='80' height='80'></td>";
     echo "<td>$nom</td>";
     echo "<td>$prenom</td>";
     echo "<td>$role</td>";
-    echo "<td><!-- Bio à ajouter --></td>";
+    echo "<td>$bio</td>";
     echo "</tr>";
 }
 
